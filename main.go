27d065ba26b8ece2150fa27f11a4d0f534dcd1f3
hirofumi omi <omihirofumi@gmail.com>
2023-07-16 18:33:39 +0900

    update main

--- main.go
+++ main.go
@@ -10,5 +10,6 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/healthl", healthLivenessHandler)
 	r.HandleFunc("/healths", healthShallowHandler)
+	r.HandleFunc("/healthd", healthDeepHandler)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
